4ae57e239f83ceed29538e359aea8589f110f565
pratik yadav <pratikyadav041@gmail.com>
2023-06-17 20:19:39 +0530

    initial commit

---
+++ git_automate.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo "Welcome to git Automation script"
+
+check_git_install(){
+	echo "checking your git version..."
+	Version=$(git --version)
+	echo "$Version"
+}
+
+check_git_install
+
+pwd
+git_init(){
+	echo "checking for git init ..."
+	sleep 04
+	if git status>/dev/null          #we can also use if [ -d .git ]
+	then
+		echo "git is already initialise"
+	else
+		echo "git is not initialise"
+		echo "Wait for initalising git into your dir"
+		git init
+		sleep 04
+		echo "git initialise succecfully"
+	fi
+}
+git_init
+
+
+git_add(){
+	git add .
+
+}
+
+git_add
+
+git_commit(){
+	echo "checking for git commit..."
+	sleep 04	
+	if git commit -m "initial commit" 2>/dev/null
+	then
+		echo "initial commit sucessfully"
+	else
+		echo "Author identity unknown"
+		read -p "please provide your github email : " email
+		read -p "please provide your name : " name
+		echo "please wait we authenticating your identity..."
+	       	git config --local user.email $email
+       		git config --local user.name $name			       
+		echo "user identify succesfully"
+
+	fi
+}
+git_commit
+
+check_remote_dir(){
+	echo "checking for remote repositry..."
+	sleep 05
+	if git remote -v | grep -q "origin"; then
+		  echo "Remote repository 'origin' already exists."
+  	else
+		  echo "Remote repository 'origin' does not exist."
+		  read -p "Enter the repo path : " repo
+		  #git remote add origin $repo
+		  echo "remote origin added sucessfully"
+
+	fi	
+}
+check_remote_dir
+
+git_push(){
+	
+	#git push origin master
+	echo "please wait we push your code on gtihub repositry"
+	echo "your code sucessfully push on github"
+
+}
+git_push
+
+
+
+
